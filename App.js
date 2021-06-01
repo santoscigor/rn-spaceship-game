0ce62804a35babc4ed77d9673c4ecfc71f91aed7
santoscigor <santoscigor@gmail.com>
2021-06-01 18:54:52 -0300

    fix: broken canva

--- App.js
+++ App.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import Canvas from 'react-native-canvas';
+import { View } from 'react-native';
 
-const handleCanvas = (canvas) => {
-  const ctx = canvas.getContext('2d');
-  ctx.fillStyle = 'purple';
-  ctx.width = 500;
-  ctx.height = 500;
-  ctx.fillRect(0, 0, 100, 100);
-}
 
 export default function App() {
   return (
-    <Canvas ref={handleCanvas}></Canvas>
+    <View></View>
   );
 }
 
