d868b5588a49d5ef3239c52542d692d720f8fa84
santoscigor <santoscigor@gmail.com>
2021-05-31 23:12:33 -0300

    feat: add canvas to app.js

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import Canvas from 'react-native-canvas';
 import { StyleSheet, Text, View } from 'react-native';
+
+const handleCanvas = (canvas) => {
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle = 'purple';
+  ctx.width = 500;
+  ctx.height = 500;
+  ctx.fillRect(0, 0, 100, 100);
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Canvas ref={handleCanvas}></Canvas>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
