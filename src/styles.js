fd594781c6523c66a5c1b7af337b0ddf21e0b523
santoscigor <santoscigor@gmail.com>
2021-05-31 22:43:23 -0300

    feat: styles

---
+++ src/styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    link_jogar: {
+        display: 'none',
+        color: 'yellow',
+        background: url(img/botao-jogar.png),
+        fontSize: 20,
+        fontFamily: 'sans-serif',
+        textDecoration: 'none',
+        texShadow: '2px 2px 5px black',
+        position: 'absolute',            
+        left: '220px',
+        top: '330px',
+        width: '52px',
+        height: '26px',
+        padding: '23px 10px',
+    }
+});
