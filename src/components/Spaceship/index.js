948e64fe98559bda118b770be90d22177bdc9199
Gabriel Andrade <gabriel.andrad4@gmail.com>
2021-06-03 22:44:18 -0300

    Add axis pad

--- src/components/Spaceship/index.js
+++ src/components/Spaceship/index.js
@@ -11,7 +11,7 @@ class Spaceship extends Component {
     render() {
         return (
             <Image
-                style={{ position: 'absolute', bottom: 0, left: this.state.left }}
+                style={{ position: 'absolute', bottom: 0, left: this.props.left }}
                 source={require('../../assets/images/nave.png')}
             />
         );
