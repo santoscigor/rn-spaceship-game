9457d9f6f80546f9f429540b7aa8ccbfc95665b8
santoscigor <santoscigor@gmail.com>
2021-06-03 21:33:20 -0300

    feat: add ovni

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import Spaceship from './src/components/Spaceship';
+import Ovni from './src/components/Ovni';
 import { styles } from './src/styles';
 
+let imagens, animacao, teclado, colisor, nave, criadorInimigos;
+let totalImagens = 0, carregadas = 0;
+let musicaAcao;
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>{'asd'}</Text>
+      <Ovni />
       <Spaceship
         left={0}
       />
