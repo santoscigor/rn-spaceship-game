6d90966e3010521713df346fc41b405fa7a16961
santoscigor <santoscigor@gmail.com>
2021-06-05 17:50:36 -0300

    refactor: set dynamic x and y to ovni

--- src/components/Ovni/index.js
+++ src/components/Ovni/index.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { Image,  } from 'react-native';
 
-const Ovni = ({ovniX}) => {
+const Ovni = ({ovniX, ovniY}) => {
     return (
         <Image
-            style={{ position: 'absolute', top: 100, left: ovniX }}
+            style={{ position: 'absolute', bottom: ovniY, left: ovniX }}
             source={require('../../assets/images/ovni.png')}
         />
     );
